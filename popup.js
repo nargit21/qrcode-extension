2709804385ad5460a607c4a4c3c3447485355fb6
nargit2106@gmail.com <nargit2106@gmail.com>
2018-10-13 22:57:32 +0400

    [upd] popup

--- popup.js
+++ popup.js
@@ -1,3 +1,19 @@
-chrome.extension.onMessage.addListener(function(message, messageSender, sendResponse) {
-	document.getElementById("qrcode").innerHTML = message.data;
-});
+function getSelectedText() {
+	chrome.tabs.executeScript({
+		code: "window.getSelection().toString()"
+	}, function (selection) {
+		if (selection[0] !== '') {
+			createQrCode(selection[0])
+		}
+	});
+}
+
+function createQrCode(data) {
+	const qrBlock = document.getElementById("qr-code-block");
+	const img = document.createElement("img");
+
+	img.src = `https://api.qrserver.com/v1/create-qr-code/?data=${data}&size=100x100x`;
+	qrBlock.appendChild(img);
+}
+
+getSelectedText()
